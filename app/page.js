bc069d7a111e10de6b800cee7e71b1365dedca91
PARSA <parsan.a360@gmail.com>
2024-05-04 22:01:56 +0600

    home button removed

--- app/page.js
+++ app/page.js
@@ -1,10 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main>
-      home page
-      <button>submit</button>
-    </main>
-  );
+  return <main>home page</main>;
 }
