6853351033d73de7350af3f23da92e64e957b9f9
PARSA <parsan.a360@gmail.com>
2024-05-06 00:07:42 +0600

    shadcn added

--- app/(root)/(home)/page.jsx
+++ app/(root)/(home)/page.jsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main>
       <h1>home prettier added page</h1>
+      <Button>Click me</Button>
     </main>
   );
 }
