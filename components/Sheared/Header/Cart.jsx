2311ae1cc5d0da27aa4ad3403a93ccef2aec4842
PARSA <parsan.a360@gmail.com>
2024-06-23 11:54:40 +0600

    new build

--- components/Sheared/Header/Cart.jsx
+++ components/Sheared/Header/Cart.jsx
@@ -1,12 +1,16 @@
-import { CarTaxiFront } from "lucide-react";
+import { CartIcon } from "@/assets/Icons/CartIcon";
 
 const Cart = () => {
-    return (
-        <>
-            <button>  <span><CarTaxiFront /></span></button>
-
-        </>
-    );
+  return (
+    <div className="flex flex-col justify-center">
+      <span className="mb-1 bg-amber-500 text-white text-center rounded-full text-xs">
+        &nbsp;4&nbsp;
+      </span>
+      <button>
+        <CartIcon height="24px" />
+      </button>
+    </div>
+  );
 };
 
 export default Cart;
--- components/Sheared/Header/Header.jsx
+++ components/Sheared/Header/Header.jsx
@@ -1,35 +1,64 @@
-import logo from "@/public/tasty-bites-kitchen-logo.png";
-
+"use client";
+// import { useRouter } from "next/router";
+import Link from "next/link";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
+import logo from "@/public/tasty-bites-kitchen-logo.png";
 import Cart from "./Cart";
 import Search from "./Search";
+import { PizzaIcon } from "@/assets/Icons/PizzaIcon";
 
 const Header = () => {
+  const isActive = usePathname();
+  // const isActive = (href) => {
+  //   return router.pathname === href;
+  // };
+
+  const headerArr = [
+    { title: "Home", link: "/" },
+    { title: "About", link: "/about" },
+    { title: "Restaurant", link: "/restaurant" },
+    { title: "Blog", link: "/blog" },
+    { title: "Contact", link: "/contact-us" },
+  ];
+
   return (
     <header className="text-gray-600 body-font">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+        <a className="flex title-font font-medium items-center mb-4 md:mb-0">
           <Image src={logo} width={200} alt="logo" />
         </a>
         <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-          <a className="mr-5 hover:text-gray-900">Home</a>
-          <a className="mr-5 hover:text-gray-900">About</a>
-          <a className="mr-5 hover:text-gray-900">Restaurants</a>
-          <a className="mr-5 hover:text-gray-900">Blog</a>
-          <a className="mr-5 hover:text-gray-900">Contact</a>
+          {headerArr.map((link, i) => (
+            <Link
+              key={i}
+              href={link.link}
+              className={`mr-5 hover:text-amber-600 relative ${isActive === `${link.link}` ? "root-primary font-medium" : "root-primary-grey font-medium"}`}
+            >
+              <span className="fixed top-[22px] ">
+                {isActive === `${link.link}` ? (
+                  <PizzaIcon width="22px" height="22px" />
+                ) : (
+                  ""
+                )}
+              </span>
+              {link.title}
+            </Link>
+          ))}
         </nav>
-        <div className="flex divide-x-[2px] divide-orange-500">
+        <div className="flex divide-x-[2px] divide-amber-500">
           <p className="p-2">
-            <span>Call For Order</span>
-            <br />
-            <span className="">1-123-000-000</span>
+            <span className="m-0 w-full block text-xs text-right">
+              Call For Order
+            </span>
+            <span className="font-extrabold text-xl root-primary">
+              1-123-000-000
+            </span>
           </p>
-          <div className="flex gap-2 p-2">
-            <Search />
+          <div className="p-2 flex gap-2 ">
+            {/* <Search /> */}
             <Cart />
           </div>
-
-
         </div>
       </div>
     </header>
