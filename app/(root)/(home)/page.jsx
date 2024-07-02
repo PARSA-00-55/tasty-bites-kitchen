161887f6bf9d4924a1129a864c20813fd6728ffa
PARSA <parsan.a360@gmail.com>
2024-07-02 09:09:14 +0600

    new carousel trying

--- app/(root)/(home)/page.jsx
+++ app/(root)/(home)/page.jsx
@@ -1,11 +1,13 @@
 import Discover from "@/components/Discover/Discover";
 import FeaturedProduct from "@/components/FeaturedProduct/FeaturedProduct";
+import HeroCarousel from "@/components/HeroSection/HeroCarousel";
 import HeroSection from "@/components/HeroSection/HeroSection";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <HeroCarousel />
       <Discover />
       <FeaturedProduct />
     </main>
---
+++ components/HeroSection/HeroCarousel.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HeroCarousel = () => {
+  return (
+    <>
+      <section className="bg-red-500">new</section>
+    </>
+  );
+};
+
+export default HeroCarousel;
